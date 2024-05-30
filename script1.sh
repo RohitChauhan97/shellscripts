4a54ffd78c62b7239f35aad6cafa73dffdb10f17
RohitChauhan97 <rhtchauhan105@gmail.com>
2024-05-30 23:45:12 +0530

    1st practise

--- script1.sh
+++ script1.sh
@@ -1,6 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
-echo "Hello batch-24, We are learning git"
-echo "Please complete your practicals on daily basis"
-
+echo "GIT BASIC PRACTICE"
+echo "DATE 10-05-2024"
